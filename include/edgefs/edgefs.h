f4682c71244363a55d3d5ea33162d70692759e19
chen-zhitao <976762403@qq.com>
2023-04-20 13:38:40 +0800

    Add readdir and getattr

--- include/edgefs/edgefs.h
+++ include/edgefs/edgefs.h
@@ -5,12 +5,14 @@
 #include <list>
 #include <map>
 #include <thread>
+#include <mutex>
 #include <atomic>
 #include <string>
 #include <cstring>
 #include <unordered_map>
 #include <cstdint>
 
+#include <stdlib.h>
 #include <fuse.h>
 
 #include "edgefs/edgerpc.h"
@@ -20,11 +22,6 @@ namespace edgefs
 
 class BitMap;
 
-enum class dentry_type {
-  REGULAR,
-  DIRECTORY
-};
-
 enum class chunck_state {
   ALIVE,
   INVAILD
@@ -32,28 +29,35 @@ enum class chunck_state {
 
 struct dentry {
   std::string d_name;
-  dentry_type d_type;
   struct inode* d_inode;
   struct dentry* d_parent;
   std::map<std::string, struct dentry*> d_childs;
 };
 
 struct inode {
-  uint64_t i_len;
-  uint64_t i_chunck_size;  // Disk chunck: 64 MB default (64 * 1024 * 1024)
-  uint64_t i_block_size;   // Memory block: 2 MB default (2 * 1024 * 1024)
-  uint64_t i_nlink;
+  uint64_t i_len;           // file length
+  uint64_t i_chunck_size;   // Disk chunck: 64 MB default (64 * 1024 * 1024)
+  uint64_t i_block_size;    // Memory block: 2 MB default (2 * 1024 * 1024)
+  uint64_t i_nlink;         // hard link number
+  uint64_t i_lock;          // file lock
+  time_t i_mtime;           // last modify time
+  time_t i_atime;           // last access time
+  mode_t i_mode;            // inode mode
+  uid_t i_uid;
+  gid_t i_gid;
 
-  BitMap* i_shard_bitmap; 
-  std::map<uint64_t, subinode*> i_subinodes; 
+  BitMap* i_shard_bitmap;   // if chunck exist 
+  std::map<uint64_t, subinode*> i_subinodes;  // chunck file inode
 };
 
 struct subinode {
-  uint64_t subi_chunk_no;
-  inode* subi_inode;
-  chunck_state subi_state;
-  BitMap* subi_block_bitmap;
+  inode* subi_inode;        // parent inode
+  uint64_t subi_no;         // chunck no
+  chunck_state subi_state;  // chunck state
+  BitMap* subi_block_bitmap;// if memcache block exist
 
+  time_t subi_atime;        // last access time
+  uint64_t subi_rcounter;   // total access times
 };
 
 struct file {
@@ -64,16 +68,15 @@ struct file {
 class EdgeFS {
 public:
   static void Init();
-  static int getattr(const char *path, struct stat *st, struct fuse_file_info *fi);
+  static int getattr(const char *path, struct stat *st);
   static int mknod(const char *path, mode_t mode, dev_t);
   static int mkdir(const char *path, mode_t mode);
   static int rmdir(const char *path);
   static int rename(const char *path1, const char *path2, unsigned int flags);
   static int read(const char *path, char *buf, std::size_t size, off_t off, struct fuse_file_info *);
   static int write(const char *path, const char *data, std::size_t size, off_t off, struct fuse_file_info *);
-  static int statfs(const char *, struct statvfs *);
-  static int readdir(
-      const char *path, void *buf, fuse_fill_dir_t filler, off_t, struct fuse_file_info *, enum fuse_readdir_flags);
+  // static int statfs(const char *, struct statvfs *);
+  static int readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offser, struct fuse_file_info * fi);
   static int open(const char *path, struct fuse_file_info *fi);
   static int unlink(const char *path);
   static int releasedir(const char *path, struct fuse_file_info *fi);
@@ -82,11 +85,13 @@ private:
   static void RPC();
   static void GC();
   static void PULL();
+  static void SCAN();
 
 private:
   static std::thread* rpc_thread_;
   static std::thread* gc_thread_;
   static std::thread* pull_thread_;
+  static std::thread* scan_thread_;
 
   static std::string center_ipv4_;
   static struct dentry* root_dentry_;
--- src/edgefs.cc
+++ src/edgefs.cc
@@ -16,8 +16,87 @@ std::thread* EdgeFS::pull_thread_ = nullptr;
 struct dentry* EdgeFS::root_dentry_ = nullptr;
 std::string EdgeFS::center_ipv4_ = std::string();
 
+void SplitPath(const char *path, std::vector<std::string>& d_names) {
+  d_names.clear();
+  uint32_t head = 0;
+  if(path[head] == '/') {
+    head++;
+  }
+
+  uint32_t len = 1;
+  while(path[head] != 0) {
+    while(path[head + len] != '/' && path[head + len] != 0) {
+      len++;
+    }
+    d_names.emplace_back(path + head, len);
+    
+    head += len;
+    if(path[head] == 0) {
+      break;
+    }
+    while(path[head] == '/') {
+      head++;
+    }
+  }
+}
+
 void EdgeFS::Init() {
 
+}
+
+int EdgeFS::getattr(const char *path, struct stat *st) {
+  LOG(INFO) << "[getattr] " << path;
+  
+  // find target dentry
+  struct dentry* target_dentry = root_dentry_;
+  std::vector<std::string> d_names;
+  SplitPath(path, d_names);
+  for(const auto& dname : d_names) {
+    auto it = target_dentry->d_childs.find(dname);
+    if(it == target_dentry->d_childs.end()) {
+      return 0;
+    }
+    target_dentry = it->second;
+  }
+
+  // stat
+  struct inode* target_inode = target_dentry->d_inode;
+  st->st_uid = target_inode->i_uid;
+  st->st_gid = target_inode->i_gid;
+  st->st_mode = target_inode->i_mode;
+  st->st_nlink = target_inode->i_nlink;
+  st->st_atime = target_inode->i_atime;
+  st->st_mtime = target_inode->i_mtime;
+}
+
+int EdgeFS::readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offser, struct fuse_file_info * fi) {
+  LOG(INFO) << "[readdir] " << path;
+  
+  // find target dentry
+  struct dentry* target_dentry = root_dentry_;
+  std::vector<std::string> d_names;
+  SplitPath(path, d_names);
+  for(const auto& dname : d_names) {
+    auto it = target_dentry->d_childs.find(dname);
+    if(it == target_dentry->d_childs.end()) {
+      return 0;
+    }
+    target_dentry = it->second;
+  }
+  
+  for(const auto& child : target_dentry->d_childs) {
+    struct stat st;
+    struct inode* target_inode = target_dentry->d_inode;
+    st.st_uid = target_inode->i_uid;
+    st.st_gid = target_inode->i_gid;
+    st.st_mode = target_inode->i_mode;
+    st.st_nlink = target_inode->i_nlink;
+    st.st_atime = target_inode->i_atime;
+    st.st_mtime = target_inode->i_mtime;
+    filler(buf, child.second->d_name.c_str(), &st, 0);
+  }
+
+  return 0;
 }
 
 void EdgeFS::RPC() {
@@ -39,5 +118,7 @@ void EdgeFS::RPC() {
   edge_server.RunUntilAskedToQuit();
   LOG(INFO) << "EdgeRPC Stop";
 }
+
+
 
 } // namespace edgefs
